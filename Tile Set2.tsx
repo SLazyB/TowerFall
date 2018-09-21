b28fffc1c3f395e3a348db23be49934dd7252153
Eric <ericjacobson1721@gmail.com>
2018-09-21 11:40:36 -0400

    new levelmap

---
+++ Tile Set2.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset2" tilewidth="16" tileheight="16" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="tilemapimagry/000.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="tilemapimagry/001.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="tilemapimagry/005.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="tilemapimagry/009.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="tilemapimagry/013.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="tilemapimagry/019.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="tilemapimagry/023.png"/>
+ </tile>
+ <tile id="48">
+  <image width="16" height="16" source="tilemapimagry/floorA.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="16" source="tilemapimagry/floorB.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="tilemapimagry/floorC.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="16" source="tilemapimagry/floorD.png"/>
+ </tile>
+</tileset>
